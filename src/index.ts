57771a5f4ae46970c917d37aaaa380d0844cf0e8
Ходосевич Матвей <hodos.952795@gmail.com>
2024-06-11 19:55:39 +0300

    добавил логику для модалки

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
 import "./style/index.scss"
 
-console.log("hello");
+const linkTryFree = document.getElementById('contact')!;
+const modal = document.getElementById('modal')!;
+const closeModal = document.getElementById('modal-close')!;
+const modalBody = document.getElementById('modal-body')!;
+
+const loadModal = () => {
+    modalBody.innerHTML = `
+        <div class="modal__title">Войти в систему</div>
+        <div class="modal__inputs">
+            <input type="text" placeholder="Email/Телефон" class="modal__input">
+            <input type="password" placeholder="Пароль" class="modal__input">
+        </div>
+        <div class="modal__check">
+            <input type="checkbox">
+            <div class="modal__check-title">Запомнить пароль</div>
+        </div>
+        <div class="modal__recover">Восстановить</div>
+        <div class="modal__btns">
+            <button class="modal__login">Войти</button>
+            <button class="modal__registration">Зарегистрироваться</button>
+        </div>`
+    ;
+}
+
+linkTryFree.addEventListener('click', () => {
+    modal.classList.toggle('open');
+    loadModal();
+});
+
+closeModal.addEventListener('click', () => {
+    modal.classList.toggle('open');
+});
